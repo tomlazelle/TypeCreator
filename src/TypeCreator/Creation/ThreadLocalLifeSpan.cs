6ab95991a4c50f5b7229481791d0ad810b153441
Tom LaZelle <tomlazelle@yahoo.com>
2015-02-27 15:49:08 -0600

    adding thread local life span

---
+++ src/TypeCreator/Creation/ThreadLocalLifeSpan.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using TypeCreator.AddStrategy;
+
+namespace TypeCreator.Creation
+{
+    public class ThreadLocalLifeSpan : DefaultLifeSpan
+    {
+        private ThreadLocal<object> _data;
+
+        private object _locked = new object();
+
+        public override object Instance(TypeContextFactory factory, IBaseTypeAction typeAction)
+        {
+
+            if (_data != null) return _data.Value;
+
+            lock (_locked)
+            {
+                _data = new ThreadLocal<object>(() => base.Instance(factory, typeAction));
+            }
+
+            return _data.Value;
+        }
+    }
+}
